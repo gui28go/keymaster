0752e6b1e6c57842d181529970e9b619db628997
gui28go <guilherme.santos1752@aluno.enova.educacao.ba.gov.br>
2026-01-02 09:21:43 -0300

    Delete firebase-firestore.js

--- firebase-firestore.js
+++
@@ -1,4 +0,0 @@
-/* Firebase Firestore (local copy) - v10.7.1 (fetched) */
-/* Content fetched from https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js */
-
-// --- START of firebase-firestore.js (placeholder) ---
