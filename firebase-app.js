c3ca8f375ea8ca213e9919e4167cf2f6be05839b
gui28go <guilherme.santos1752@aluno.enova.educacao.ba.gov.br>
2026-01-02 09:21:34 -0300

    Delete firebase-app.js

--- firebase-app.js
+++
@@ -1,4 +0,0 @@
-/* Firebase App (local copy) - v10.7.1 (minified/esm content saved) */
-/* Content fetched from https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js */
-
-// --- START of firebase-app.js (truncated provenance content) ---
